56b9d9c1a0785fd0f081a1ee3ad79e249e6b260b
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-10-29 14:19:15 +0530

    services.tf

---
+++ part3_ecr_ecs/services.tf
@@ -0,0 +1,35 @@
+resource "aws_ecs_service" "flask_service" {
+  name            = "flask-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.flask_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets         = [aws_subnet.public_a.id]
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.flask_tg.arn
+    container_name   = "flask"
+    container_port   = 5000
+  }
+}
+
+resource "aws_ecs_service" "express_service" {
+  name            = "express-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.express_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets         = [aws_subnet.public_a.id]
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.express_tg.arn
+    container_name   = "express"
+    container_port   = 3000
+  }
+}
