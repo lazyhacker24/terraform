ba74e5ffd0de142f52895172a25bef3a1c1463f0
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-10-29 14:16:53 +0530

    variables.tf

---
+++ part2_two_ec2/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "ami_id" {
+  default = "ami-0dee22c13ea7a9a67"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "your-keypair-name"
+}
