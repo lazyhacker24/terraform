6396126ed7109a3171661b0b27b3c9a1a766bc38
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-10-29 14:18:06 +0530

    vpc.tf

---
+++ part3_ecr_ecs/vpc.tf
@@ -0,0 +1,49 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = { Name = "ecs-vpc" }
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs-service-sg"
+  vpc_id      = aws_vpc.main.id
+
+  ingress = [
+    {
+      from_port   = 0
+      to_port     = 65535
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress = [{
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }]
+}
